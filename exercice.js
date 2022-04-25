93c721cb2fde669f1babbcc4c42c003a38473a4a
bloullittaa <bloullittaa@gmail.com>
2022-04-25 16:07:29 +0200

    init file

---
+++ exercice.js
@@ -0,0 +1,13 @@
+
+console.log(cercle(2));
+
+
+
+console.log("1 janvier est un dimanche " + year);
+
+console.log(mot.substring(1,2));
+
+console.log(lettres);
+
+
+// console.log(m);
---
+++ main.js
@@ -0,0 +1,36 @@
+let cercle = 20;
+function cercle(R){
+    return Math.PI *(R * R);
+}
+import cercle from "excercice.js";
+
+
+
+
+for (let année = 2014; année <= 2015; année++)
+{
+    let date = newDate(année,0 , 1);
+    if (date.get() === 0);
+    
+}
+
+import année from "excercice.js";
+
+
+
+
+let mot = Fibonnaci;
+import Fibonnaci from "excercice.js";
+
+
+
+
+
+let m = Brutforce;
+let lettres = [];
+let division = m.split('');
+for (let division = 0; division.length > 0; division.length < 25; division++) ;
+
+import lettres from "excercice.js";
+import division from "excercice.js";
+import m from "excercice.js";
